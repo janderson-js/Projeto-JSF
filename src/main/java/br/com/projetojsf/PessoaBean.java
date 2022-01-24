bdb2da7c7589b0c83299aa6a5592b76bc7a3f7af
jandy <jandy@DESKTOP-90FV4KB>
2022-01-24 13:45:23 -0300

    AULA - Expression Language JSF

--- src/main/java/br/com/projetojsf/PessoaBean.java
+++ src/main/java/br/com/projetojsf/PessoaBean.java
@@ -4,29 +4,17 @@ import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
 
+import lombok.Data;
+
 @ManagedBean(name = "pessoaBean")
+@Data
 public class PessoaBean implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	private String nome;
 	private String sobrenome;
 
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getSobrenome() {
-		return sobrenome;
-	}
-
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
-	}
 
 }
 
