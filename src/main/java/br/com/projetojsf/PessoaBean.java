fc49da1f16a030e6506c91935c183908e814b5d1
jandy <jandy@DESKTOP-90FV4KB>
2022-01-24 14:03:48 -0300

    AULA - Ligando atributos com expression language jsf

--- src/main/java/br/com/projetojsf/PessoaBean.java
+++ src/main/java/br/com/projetojsf/PessoaBean.java
@@ -4,17 +4,43 @@ import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
 
-import lombok.Data;
-
 @ManagedBean(name = "pessoaBean")
-@Data
 public class PessoaBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private String nome;
 	private String sobrenome;
+	private String nomeCompleto;
 
+	public String mostrarNome() {
+		nomeCompleto = nome + " " + sobrenome;
+		return "";
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
 
 }
 
