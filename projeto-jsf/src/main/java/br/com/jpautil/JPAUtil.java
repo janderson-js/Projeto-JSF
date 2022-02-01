dcfb70c6efc6a41ed5fb54b68c188ebe77c23f93
jandy <jandy@DESKTOP-90FV4KB>
2022-01-31 21:31:51 -0300

    AULA - Classe JPAUtil

---
+++ projeto-jsf/src/main/java/br/com/jpautil/JPAUtil.java
@@ -0,0 +1,21 @@
+package br.com.jpautil;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+	
+	private static EntityManagerFactory factory;
+	
+	static {
+		if(factory == null){
+			factory = Persistence.createEntityManagerFactory("projeto-jsf");
+		}
+	}
+	
+	public static EntityManager getEntityManager() {
+		return factory.createEntityManager();
+	}
+	
+}
