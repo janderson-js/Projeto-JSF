728cb57eaddf71fb534ca6ad07b5387f0ea0ac1d
jandy <jandy@DESKTOP-90FV4KB>
2022-01-31 21:24:20 -0300

    AULA - Testando o JPA e a criação de tabelas

---
+++ projeto-jsf/src/test/java/br/com/projetojsf/TesteJPA.java
@@ -0,0 +1,9 @@
+package br.com.projetojsf;
+
+import javax.persistence.Persistence;
+
+public class TesteJPA {
+	public static void main(String[] args) {
+		Persistence.createEntityManagerFactory("projeto-jsf");
+	}
+}
