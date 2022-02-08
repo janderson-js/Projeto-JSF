b92a2fc9a95bcddccdc7bba8662724d4d3f65e80
jandy <jandy@DESKTOP-90FV4KB>
2022-02-08 12:59:35 -0300

    AULA - Transformando o cadastro de pessoa em usuário do sistema

--- projeto-jsf/src/main/java/br/com/entidades/Pessoa.java
+++ projeto-jsf/src/main/java/br/com/entidades/Pessoa.java
@@ -30,6 +30,12 @@ public class Pessoa implements Serializable {
 	
 	private String sexo;
 	
+	private String login;
+	
+	private String senha;
+	
+	private String perfilUser;
+	
 	private String[] fremeWork;
 	
 	private Boolean ativo;
@@ -103,6 +109,30 @@ public class Pessoa implements Serializable {
 
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public String getPerfilUser() {
+		return perfilUser;
+	}
+
+	public void setPerfilUser(String perfilUser) {
+		this.perfilUser = perfilUser;
 	}
 
 	@Override
